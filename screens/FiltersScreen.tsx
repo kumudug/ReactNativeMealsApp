177c8d3b5dfd81091caebd93f45dee0756adf214
kumudug <kumudug1@gmail.com>
2020-11-01 10:23:41 -0800

    Boilerplate Screens
    * Components for all the screens with boilterplate code

---
+++ screens/FiltersScreen.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+type AppProps = {
+
+};
+
+const FiltersScreen = (props: AppProps) => {
+    return (
+        <View style={styles.screen}>
+            <Text>Category Meal Screen!</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default FiltersScreen;

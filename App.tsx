2c98e3aea4f58ba9316fd1d27b289fa6ecedfdf1
kumudug <kumudug1@gmail.com>
2020-11-01 10:43:07 -0800

    Adding Custom fonts
    * Use expo-fonts
    * Used AppLoading to load fonts first

--- App.tsx
+++ App.tsx
@@ -1,8 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// Installed this package using expo install expo-font to make sure to install the right version to match expo version
+import * as Font from 'expo-font'; 
+import { AppLoading } from 'expo'; // To prolong loading screen until fonts are loaded
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+}
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState<boolean>(false);
+
+  if (!fontLoaded) {
+    return <AppLoading
+      startAsync={fetchFonts}
+      onFinish={() => { setFontLoaded(true) }}
+      onError={(err) => console.log(err)} />
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>

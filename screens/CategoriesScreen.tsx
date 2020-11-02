d0e71d9d97f7d93df1cd93a0f6568528dbb62f29
kumudug <kumudug1@gmail.com>
2020-11-02 09:04:09 -0800

    Add a navigate button to test

--- screens/CategoriesScreen.tsx
+++ screens/CategoriesScreen.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { NavigationScreenProp } from 'react-navigation'
 
 type AppProps = {
-
+    navigation: NavigationScreenProp<any, any>
 };
 
 const CategoriesScreen = (props: AppProps) => {
+    console.log(props);
     return (
         <View style={styles.screen}>
             <Text>Categories Screen!</Text>
+            <Button title="Go to Meals" onPress={() => {
+                props.navigation.navigate({routeName: 'CategoryMeals'});
+            }} />
         </View>
     );
 };
